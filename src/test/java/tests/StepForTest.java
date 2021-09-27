d7dc088c45d54eab897c76c7ebf1e0b566864ad3
vsavvina <82946852+vsavvina@users.noreply.github.com>
2021-09-27 21:56:37 +0300

    + TestWithAnntStap

--- src/test/java/tests/StepForTest.java
+++ src/test/java/tests/StepForTest.java
@@ -1,10 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,13 +19,15 @@ public class StepForTest {
   public void searchrep (String repozit) {
     $(".header-search-input").setValue(repozit).submit();
   }
+
   @Step("Going to the first search result")
   public void goToFirstRes () {
     $$("ul.repo-list li").first().$("a").click();
   }
+
   @Step("Checking the visibility of Issues")
   public void checkVisib () {
     $x("//*[@id=\"repository-container-header\"]/nav/ul/li[2]")
-            .should(visible);
+            .should(Condition.visible);
   }
 }
--- src/test/java/tests/TestWithAnntStap.java
+++ src/test/java/tests/TestWithAnntStap.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class TestWithAnntStap {
 
-  public final static String repository = "vsavvina/qa_class_6";
-  public final static String gitUrl = "https://github.com/";
   StepForTest stepfortest = new StepForTest();
+  public String repository = "vsavvina/qa_class_6";
+  public String gitUrl = "https://github.com/";
 
   @Test
-  public void IssTestGit() {
+  void IssTestGit() {
     stepfortest.openinGitHub(gitUrl);
     stepfortest.searchrep(repository);
     stepfortest.goToFirstRes();
